18708ba3f6d9ef906de8bc7a224896a9d714b6f7
idoasisay <ihannah@kakao.com>
2022-11-11 18:03:55 +0900

    Delete basic view

---
+++ client/src/components/Header.vue
@@ -0,0 +1,22 @@
+<template><div>hello</div></template>
+<script>
+export default {
+  name: 'header-component',
+  components: {},
+  data() {
+    return {
+      sampleData: '',
+    };
+  },
+  setup() {},
+  beforeCreate() {},
+  created() {},
+  beforeMount() {},
+  mounted() {},
+  beforeUpdate() {},
+  updated() {},
+  beforeUnmount() {},
+  unmounted() {},
+  methods: {},
+};
+</script>
