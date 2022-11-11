ef058ea6a488452478fcce2c1be4413d8c04ff1d
idoasisay <ihannah@kakao.com>
2022-11-11 19:49:27 +0900

    main view init

---
+++ client/src/components/HeaderText.vue
@@ -0,0 +1,6 @@
+<template><p>지브리 3D 엽서(?)를 위한 POC</p></template>
+<script>
+export default {
+  name: "header-text",
+};
+</script>
--- client/src/views/MainView.vue
+++ client/src/views/MainView.vue
@@ -1,8 +1,14 @@
-<template><div></div></template>
+<template>
+  <div>
+    <HeaderText />
+    <div>여기에 작성하세요.</div>
+  </div>
+</template>
 <script>
+import HeaderText from "../components/HeaderText.vue";
 export default {
   name: "main-view",
-  components: {},
+  components: { HeaderText },
   data() {
     return {
       sampleData: "",
