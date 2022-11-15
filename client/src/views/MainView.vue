0c8f05c75c352c26fec9b73048b5bbf49da06035
idoasisay <ihannah@kakao.com>
2022-11-15 17:22:50 +0900

    pratice updown rotate

--- client/src/views/MainView.vue
+++ client/src/views/MainView.vue
@@ -3,6 +3,7 @@
     <HeaderText />
     <div>
       <div class="circle" ref="cir" @mousemove="trans"></div>
+      <div class="square" ref="squ" @mousemove="skew"></div>
     </div>
   </div>
 </template>
@@ -30,6 +31,11 @@ export default {
       this.$refs.cir.style.setProperty("--mouse-x", e.clientX);
       this.$refs.cir.style.setProperty("--mouse-y", e.clientY);
     },
+    skew: function (e) {
+      this.$refs.squ.style.setProperty("--mouse-x", e.clientX);
+      this.$refs.squ.style.setProperty("--mouse-y", e.clientY);
+      console.log(e.clientY);
+    },
   },
 };
 </script>
@@ -41,7 +47,14 @@ export default {
   background-color: rosybrown;
   transform-origin: center;
   transform: translateX(calc(var(--mouse-x) * 0.3px))
-    translateY(calc(var(--mouse-y) * 0.3px));
+    translateY(calc(var(--mouse-y) * 0.1px));
   transition: transform 0.5 ease;
+}
+.square {
+  width: 100px;
+  height: 100px;
+  background-color: red;
+  transform: perspective(150px) rotateX(calc(var(--mouse-y) * 0.1deg));
+  /* transition: transform 0.5 ease; */
 }
 </style>
